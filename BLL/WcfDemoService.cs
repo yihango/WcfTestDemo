3c6bfc32252a5114c8565b67cd06239f24b95d23
Stan E <Stan E>
2017-04-07 10:51:54 +0800

    添加项目文件。

---
+++ BLL/WcfDemoService.cs
@@ -0,0 +1,21 @@
+﻿using IBLL;
+
+namespace BLL
+{
+    /// <summary>
+    /// 实现服务契约业务类
+    /// </summary>
+    public class WcfDemoService : IWcfDemoService
+    {
+        /// <summary>
+        /// 实现操作契约函数
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ IBLL/IWcfDemoService.cs
@@ -0,0 +1,21 @@
+﻿using System.ServiceModel;
+
+namespace IBLL
+{
+    /// <summary>
+    /// 服务契约接口
+    /// </summary>
+    [ServiceContract]
+    public interface IWcfDemoService
+    {
+        /// <summary>
+        /// 一个操作契约
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        [OperationContract]
+        int Add(int a, int b);
+
+    }
+}
